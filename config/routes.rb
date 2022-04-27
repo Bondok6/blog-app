e592618702580c699aaccea331d4993521504589
Bondok6 <kirolousbondok6@gmail.com>
2022-04-27 11:24:32 +0200

    Add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get '/users/', to: 'users#index'
+  get '/users/:id', to: 'users#show'
 end
