8043336b14f2cc64b11d34255d2ddee2fe533f71
Bondok6 <kirolousbondok6@gmail.com>
2022-05-03 05:07:19 +0200

    Create recent_comments and update_posts_counter methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,14 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  def recent_comments 
+    comments.last(5)
+  end
+
 end
