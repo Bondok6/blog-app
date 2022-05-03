9d3d07af6c9174f3668950328dbf24a7fef2e1f8
Bondok6 <kirolousbondok6@gmail.com>
2022-05-03 05:08:32 +0200

    Create update_post_comments_counter method

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,10 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_save :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.increment!(:comments_counter)
+  end
+
 end
