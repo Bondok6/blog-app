b059b12fe28495a1b357a71fa48e27e3ac3e56ca
Bondok6 <kirolousbondok6@gmail.com>
2022-05-04 17:39:30 +0200

    Add Design to users pages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @posts = Post.all
+  end
 
-  def show; end
+  def show
+    @posts = Post.find(params[:id])
+  end
 end
