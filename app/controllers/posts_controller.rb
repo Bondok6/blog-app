788cb3c553fe10150b0238be3c4aa8a94c9a9b54
Bondok6 <kirolousbondok6@gmail.com>
2022-05-05 09:54:53 +0200

    Create new post on behalfof the current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,23 @@ class PostsController < ApplicationController
   def show
     @post = User.find(params[:user_id]).posts.find(params[:id])
   end
+
+  def new 
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.new(post_params)
+    if @post.save
+      redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
+    else
+      render :new 
+    end
+  end
+
+  private 
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
