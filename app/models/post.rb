6686c115702d6fe3e846889cf440c9e82517d6a1
Bondok6 <kirolousbondok6@gmail.com>
2022-05-06 08:20:03 +0200

    Add validation to User and Post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
@@ -12,4 +16,5 @@ class Post < ApplicationRecord
   def recent_comments
     comments.last(5)
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
   has_many :posts, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
   has_many :comments, foreign_key: 'author_id'
