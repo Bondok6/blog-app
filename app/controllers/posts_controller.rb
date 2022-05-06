80de7cb5cc5b62fbabae7da726339c7d695d4c04
Bondok6 <kirolousbondok6@gmail.com>
2022-05-06 10:12:14 +0200

    Add Flash messages

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,7 +13,10 @@ class PostsController < ApplicationController
 
   def create
     @post = current_user.posts.new(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
     if @post.save
+      flash[:success] = "Post has been added successfully"
       redirect_to "/users/#{@post.author.id}/posts/#{@post.id}"
     else
       render :new
