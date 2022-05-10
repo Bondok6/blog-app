97440431d3e2942e3cbd30b5dc22f71deb3207a9
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-10 09:26:51 -0600

    Specify root routes for auth or !auth user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "users#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'users#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 
   resources :users do
     resources :posts do
@@ -11,4 +19,6 @@ Rails.application.routes.draw do
   end  
 
   resources :posts
+
+  root "users#index"
 end
