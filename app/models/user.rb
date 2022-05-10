326625156132294fceb546465d0ca20d7f1a7c1d
eduardosancho <53308506+eduardosancho@users.noreply.github.com>
2022-05-10 10:06:18 -0600

    Allow to destroy user even if it has children

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
   # validates :name, presence: true
   # validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
 
   def recent_posts
     posts.last(3)
