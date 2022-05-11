a872236e6bf1e62a3ff23370d9e8d2d16f516dbc
Bondok6 <kirolousbondok6@gmail.com>
2022-05-11 12:06:42 +0200

    Fix linters

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -36,12 +36,13 @@ class Ability
 
     # Users:
     return unless user.present?
+
     can :destroy, Post, author_id: user.id
     can :destroy, Comment, author_id: user.id
 
     # Admins:
     return unless user.admin?
-    can :manage, :all
 
+    can :manage, :all
   end
 end
