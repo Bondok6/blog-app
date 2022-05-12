8c14703c0cfd0c1552a8a78d64031ded09a9b385
Bondok6 <kirolousbondok6@gmail.com>
2022-05-12 02:04:53 +0200

    Fix linters

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -20,14 +20,16 @@ RSpec.describe 'Login', type: :system do
       expect(page).to have_content('Invalid Email or password.')
     end
 
-    it 'When I click the submit button after filling in the username and the password with incorrect data, I get a detailed error' do
+    it 'When I click the submit button after filling in the username and the password with incorrect data,
+    I get a detailed error' do
       page.fill_in 'Email', with: 'anything@gmail.com'
       page.fill_in 'Password', with: '555555'
       click_button 'Log in'
       expect(page).to have_content('Invalid Email or password.')
     end
 
-    it 'When I click the submit button after filling in the username and the password with correct data, I am redirected to the root page' do
+    it 'When I click the submit button after filling in the username and the password with correct data,
+    I am redirected to the root page' do
       page.fill_in 'Email', with: 'edu@gmail.com'
       page.fill_in 'Password', with: '123456'
       click_button 'Log in'
